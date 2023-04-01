898cd1783fef5ef6d2ee3f9d62628d8cea8c16e4
Hans Wang <hansmikaa@live.cn>
2023-04-01 09:34:11 -0400

    stdin setup

--- play.js
+++ play.js
@@ -5,4 +5,27 @@ const net = require("net");
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //handleUserInput and register it as the "data" callback handler for stdin.
+  const handleUserInput = function (data) {
+  };
+  
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+
+
+
+
+};
+
+
+
 
