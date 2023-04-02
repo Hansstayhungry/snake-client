17ecab2a8373453ae1416789189263513d7dd49c
Hans Wang <hansmikaa@live.cn>
2023-04-02 17:08:15 -0400

    WASD movement

--- input.js
+++ input.js
@@ -1,2 +1,38 @@
-const { setupInput } = require("./play");
+// Stores the active TCP connection object.
+let connection;
+
+// setup interface to handle user input from stdin
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //handleUserInput and register it as the "data" callback handler for stdin.
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    } else if (data === 'w') {
+      connection.write("Move: up");
+    } else if (data === 'a') {
+      connection.write("Move: left");
+    } else if (data === 's') {
+      connection.write("Move: down");
+    } else if (data === 'd') {
+      connection.write("Move: right");
+    };
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+module.exports = { setupInput };
+
+
+
 
--- play.js
+++ play.js
@@ -4,44 +4,9 @@ const { setupInput } = require("./input");
 const net = require("net");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
-setupInput();
+setupInput(conn);
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  //handleUserInput and register it as the "data" callback handler for stdin.
-
-  const handleUserInput = function (data) {
-    if (data === 'p') {
-      process.exit();
-    } else if (data === 'w') {
-      connect.write('Move: up');
-    } else if (data === 's') {
-      connect.write('Move: down');
-    } else if (data === 'a') {
-      connect.write('Move: left');
-    } else if (data === 'd') {
-      connect.write('Move: right');
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-//call setupInput
-setupInput();
-
-module.exports = {
-  setupInput
-};
-
 
 
